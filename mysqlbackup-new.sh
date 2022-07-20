bbd44b055797064f5e8364b3a89592fd4abc9c04
inextkb <74397736+iNextKB@users.noreply.github.com>
2022-07-20 12:05:04 +0530

    initial committed

---
+++ mysqlbackup-new.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# Creqated By Gayantha
+
+echo "starting db backup"
+
+read -p "Please Enter your DB user :- " dbuser
+read -p "Please Enter your DB user password :- " password
+mysql -u $dbuser -p$password -e "show databases";
+echo
+read -p "Please Enter your DB Name :- " dbname
+day="$(date +'%d-%m-%Y_%H-%M-%S')"
+db_backup="${dbname}_${day}.sql"
+mysqldump  -u $dbuser -p$password $dbname > ./${db_backup}
+
+if [ $? == 0 ]; then
+echo "db backup completed"
+fi
+#Compress backup
+
+gzip -c ./${db_backup} > ./${db_backup}.gz
+if [ $? == 0 ]; then
+  echo 'The backup was successfully compressed'
+else
+  echo 'Error compressing backup'
+ exit
+fi
+ls -laSh
